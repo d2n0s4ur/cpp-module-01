8803951bed06475dcdd3d3c2e9728026634cfc4d
Junho Noh <jnoh@cx1r1s2.42seoul.kr>
2023-01-20 17:29:29 +0900

    fix: ex03 seg fault

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: jnoh <jnoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:23:14 by jnoh              #+#    #+#             */
-/*   Updated: 2023/01/12 16:41:01 by jnoh             ###   ########.fr       */
+/*   Updated: 2023/01/20 17:28:47 by jnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 HumanB::HumanB( std::string name  )
 {
 	this->name_ = name;
+	this->weapon_ = 0;
 }
 
 HumanB::~HumanB()
@@ -29,5 +30,8 @@ void	HumanB::setWeapon( Weapon &weapon )
 
 void	HumanB::attack( void )
 {
-	std::cout << this->name_ << " attacks with their " << this->weapon_->getType() << std::endl;
+	if (this->weapon_)
+		std::cout << this->name_ << " attacks with their " << this->weapon_->getType() << std::endl;
+	else
+		std::cout << this->name_ << " attacks with their " << "empty hands" << std::endl;
 }
